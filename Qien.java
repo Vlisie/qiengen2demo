3429d44deaf1c1f5797b6f6ad90db145a64aaa4c
Vlisie <tesvdvlist@gmail.com>
2018-07-02 11:37:29 +0200

    hoi

---
+++ Qien.java
@@ -0,0 +1,19 @@
+class Qien{
+	public static void main(String[] args){
+		System.out.println("de tekst die je wilt typen");
+
+		int leeftijd = 14;
+		
+
+	
+		if(leeftijd < 16){
+			System.out.println("Helemaal niks");
+		}
+		if(leeftijd >= 16 && leeftijd < 18){
+			System.out.println("Alleen bier en wijn");
+		}
+		if(leeftijd > 18){
+			System.out.println("Alles mag");
+		}
+	}
+}
